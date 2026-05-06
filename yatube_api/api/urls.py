f2a1a490a70cecd0b8232fdd8d4883699db72523
leonovamp06-afk <leonovamp06@gmail.com>
2026-05-06 20:23:29 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,7 +8,11 @@ router = DefaultRouter()
 router.register('posts', PostViewSet)
 router.register('groups', GroupViewSet)
 router.register('follow', FollowViewSet, basename='follow')
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comment')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comment'
+)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
