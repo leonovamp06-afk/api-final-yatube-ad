aeaec5c7e070aacf9bef3ed6d5f3a8c62184bbbc
leonovamp06-afk <leonovamp06@gmail.com>
2026-05-06 20:31:04 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken import views
 
 from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
 
@@ -16,5 +15,5 @@ router.register(
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/jwt/', include('rest_framework_simplejwt.urls')),
 ]
