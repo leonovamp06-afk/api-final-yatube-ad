291c0f1ff3fdc344c2ee8f4de00dd066376022d7
leonovamp06-afk <leonovamp06@gmail.com>
2026-05-06 12:54:46 +0300

    Update models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -24,6 +24,8 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
 class Follow(models.Model):
     user = models.ForeignKey(
         User,
