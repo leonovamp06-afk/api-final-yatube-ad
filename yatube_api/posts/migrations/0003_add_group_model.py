dd4d7dcf3953ed0a8fbd11e232c5756f97535f9e
leonovamp06-afk <leonovamp06@gmail.com>
2026-05-06 13:03:21 +0300

    Create 0003_add_group_model.py

---
+++ yatube_api/posts/migrations/0003_add_group_model.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('posts', '0002_auto_20260506_1017'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('slug', models.SlugField(unique=True)),
+                ('description', models.TextField()),
+            ],
+        ),
+    ]
